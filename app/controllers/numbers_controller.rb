3694e873ca6aae9218f9670b90685869c4620a7f
michaelhf <michaelhf@gmail.com>
2015-04-29 21:04:28 -0500

    more stuff

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,11 @@ class NumbersController < ApplicationController
   def unlucky_numbers
     @numbers = [rand(1..100), rand(1..100), rand(1..100), rand(1..100), rand(1..100)]
   end
+  def random_numbers
+   number = params["num"].to_i || 1000
+   # number =  params["num"].present? ? params["num"].to_i : 1000
+   number = params.fetch(:num,1000).to_i
+    # raise number.inspect
+    @the_number = rand(number)
+  end
 end
